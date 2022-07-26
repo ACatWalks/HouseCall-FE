37ca51832321668782cc195dc4d2dfb81b0f601e
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-07-26 13:40:30 -0700

    Now displays a banner

--- src/view/NavBar.js
+++ src/view/NavBar.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link, Outlet} from 'react-router-dom'
+import { useParams } from 'react-router'
 
 function NavBar() {
+
+    const userId = useParams()
+
+    const [user, setUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        role: ''
+    })
+
+    const [patientProfileImage, setPatientProfileImage] = useState('')
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const response = await fetch(`http://localhost:4000/patients/${userId}`)
+                const resData = await response.json()
+                setUser(resData)
+                setPatientProfileImage(resData.patientProfileImage)
+            } catch {
+                const response = await fetch(`http://localhost:4000/medical-provider/${userId}`)
+                const resData = await response.json()
+                setUser(resData)
+            }
+        }
+    }, [ userId ])
+
+    function handleRole(role) {
+        if(role === 'Doctor'){
+            return (
+            <li>
+                Welcome Dr. {user.firstName} {user.lastName}
+            </li>
+            )
+        } else if(role === 'Patient'){
+            return (
+            <li>
+                You are logged in as {user.firstName} {user.lastName}
+            </li>
+            )
+        } else{
+            return (
+                <div>
+                    You must be logged in.
+                </div>
+
+            )
+        }
+    }
+
+    function pic() {
+        if(patientProfileImage){
+            return (
+                <div>
+                    <img src={patientProfileImage} />
+                </div>
+            )
+        }
+    }
+
     return (
         <div className='nav'>
             <ul>
@@ -12,6 +74,8 @@ function NavBar() {
                 <li><Link to="/symptoms">Report Symptoms</Link></li>
                 <li><Link to="/chat">Chat With a Doctor</Link></li>
             </ul>
+            {handleRole(user.role)}
+            {pic}
             <Outlet />
         </div>
     )
