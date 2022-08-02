c7387a637667527db1df77e571eddd6afd90712a
LuckyShmucky <christianluciano567@gmail.com>
2022-08-02 12:21:45 -0700

    Features/Post request to create a new chat between a patient and a doctor is working. Goals/ We need to display the text in the chat to the users

--- src/view/Chat.js
+++ src/view/Chat.js
@@ -13,56 +13,16 @@ function Chat() {
                  No chat items yet.
         </h3>
     )
-    function fillDoctorOrPatient() {
-        if(userRole === 'Doctor') {
-            setChats({...chats, doctor: userId})
-        } else {
-            setChats({ ...chats, patient: userId })
-        }
-    }
-    let commentArr = []
-    const [chats, setChats] = useState({
-        doctor: '62e8599a47803539729cb7d4',
-        patient: '62e85a0b47803539729cb7d7',
-        content: []
-    })
-    async function createMessage(messageAttributes) {
-        setChats({...chats, content: commentArr})
-        fillDoctorOrPatient()
-        const chat = await fetch(`http://localhost:4000/chats/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(chats)
-        })
-        const res = await chat.json()
-        const response = await fetch(`http://localhost:4000/chats/messages/${chatId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(messageAttributes)
-        })
-        const message = await response.json()
-        commentArr.push(message)
-    }
-
-    if(commentArr.length) {
-        comments = commentArr.map(message => {
-            return (
-                <ChatCard key={message.commentId} message={message} />
-            )
-        })
-    } 
-
+    
+    
+    
     return (
         <main>
             <NavBar />
-            <h2>Chat With a Doctor</h2>
-            {comments}
+            <h2>Enter A Doctor's License Number</h2>
+            {/* {comments} */}
             <hr />
-            <NewChat onSubmit={createMessage} />
+            <NewChat  />
         </main>
     )
 }
--- src/view/NewChat.js
+++ src/view/NewChat.js
@@ -5,34 +5,53 @@ function NewChat({ onSubmit }) {
     const author = sessionStorage.getItem('firstName') + ' ' + sessionStorage.getItem('lastName')
 
     const userRole = sessionStorage.getItem('role')
+    const userId = sessionStorage.getItem('id')
+    // const [message, setMessage] = useState({
+    //     text: '',
+    //     author: author,
+    //     onModel: userRole
+    // })
 
-    const [message, setMessage] = useState({
-        text: '',
-        author: author,
-        onModel: userRole
-    })
-
-    function handleSubmit(e) {
+    let reqBody = {
+        doctor: '',
+        patient: ''
+    }
+    function setOtherUserId(e) {
         e.preventDefault()
-        onSubmit(message)
-        setMessage({
-            text: '',
-            author: author,
-            onModel: userRole
+        reqBody.doctor = e.target.value 
+    }
+
+    if(userRole === 'Patient'){
+        reqBody.patient = userId
+    } else {
+        reqBody.doctor = userId
+    }
+   
+    async function createChat(e) {
+        e.preventDefault()
+
+        const chat = await fetch(`http://localhost:4000/chats/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
         })
+        const res = await chat.json()
+        console.log(res)
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={e => createChat(e)}>
             <div className='row'>
-            <label htmlFor="content">Content</label>
-            <textarea required id="content" name="content" value={message.text} onChange={e => setMessage({...message, text: e.target.value})} />
+            <label htmlFor="content">Enter Medical License</label>
+            <textarea required id="content" name="content" onChange={e => setOtherUserId(e)}  />
             </div>
             <div className='row'>
-            <label htmlFor="author">Author: {author}</label>
+            <label htmlFor="author">Author:</label>
             </div>
             <div className='row'>
-            <input type="submit" value="Reply" className="form-btn" />
+            <input type="submit" className="form-btn" />
             </div>
         </form>
     )
