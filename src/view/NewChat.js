a4dd395d4aef5360b72dc017333032ad7f2f4b83
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-08-01 22:12:57 -0700

    Fetch request returns a fulfilled promise, but messages is undefined

--- src/view/NewChat.js
+++ src/view/NewChat.js
@@ -2,22 +2,25 @@ import { useState } from "react"
 
 function NewChat({ onSubmit }) {
 
-    const author = sessionStorage.getItem('firstName') + ' ' + sessionStorage.getItem('lastName')
+    const by = sessionStorage.getItem('firstName') + ' ' + sessionStorage.getItem('lastName')
 
     const userRole = sessionStorage.getItem('role')
+    const userId = sessionStorage.getItem('id')
 
     const [message, setMessage] = useState({
         text: '',
-        author: author,
+        author: userId,
         onModel: userRole
     })
 
     function handleSubmit(e) {
         e.preventDefault()
+        sessionStorage.setItem('text', message.text)
         onSubmit(message)
+        sessionStorage.removeItem('text')
         setMessage({
             text: '',
-            author: author,
+            author: userId,
             onModel: userRole
         })
     }
@@ -29,7 +32,7 @@ function NewChat({ onSubmit }) {
             <textarea required id="content" name="content" value={message.text} onChange={e => setMessage({...message, text: e.target.value})} />
             </div>
             <div className='row'>
-            <label htmlFor="author">Author: {author}</label>
+            <label htmlFor="author">Author: {by}</label>
             </div>
             <div className='row'>
             <input type="submit" value="Reply" className="form-btn" />
